d27c7c929ccb6325eadfd44c8d7a4ccae88ae099
LucianoGLR <lucianogallinger@gmail.com>
2024-04-29 16:26:17 -0300

    add vercel

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const axios = require("axios");
 const server = require("./src/server");
 const { conn } = require('./src/db.js');
-const PORT = 3001;
+const port = process.env.PORT || 3001;
 const { saveCountriesInDB } = require('./src/controllers/countriesControllers.js')
 
 conn.sync({ force: true }).then(() => {
-server.listen(PORT, () => {
+server.listen(port, () => {
   saveCountriesInDB();
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${port}`);
 })
 }).catch(error => console.error(error))
